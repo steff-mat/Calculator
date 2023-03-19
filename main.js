40f4b0303bfe6a4665fcf26ff7dd25b3767a2ff3
Stefan Maties <maties.stefan44@gmail.com>
2023-03-19 23:29:25 +0100

    Small improvement

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ const btnDot = document.getElementById("dot");
 const btnAC = document.getElementById("ac");
 const btnSwap = document.getElementById("swap");
 let currentCalculation = document.getElementById("cc");
-let ccText = currentCalculation.innerText;
 let oldCalculation = document.getElementById("oc");
-
-const add = function (a, b) {
-  return a + b;
+let mathResult;
+const valueHold = {
+  numberOne: "",
+  numberTwo: "",
+  result: "",
 };
+
+function add(a, b) {
+  return a + b;
+}
 function subtract(a, b) {
   return a - b;
 }
@@ -33,76 +38,157 @@ function operate(operator, a, b) {
   }
 }
 
-function handler() {
-  let arr = [];
-  let arr1;
-  let arr2;
-  let temp = currentCalculation.innerText;
-  let output;
-  if (temp.includes("+")) {
-    arr = temp.split("+");
-    arr1 = parseFloat(arr[0]);
-    arr2 = parseFloat(arr[1]);
+// OLD FUNCTION - LOGIC TOTALLY BROKEN
+// function handler() {
+//   let arr = [];
+//   let arr1;
+//   let arr2;
+//   let temp = currentCalculation.innerText;
+//   let output;
+//   if (temp.includes("+")) {
+//     arr = temp.split("+");
+//     arr1 = parseFloat(arr[0]);
+//     arr2 = parseFloat(arr[1]);
 
-    return (output = operate("+", arr1, arr2));
-  } else if (temp.includes("-")) {
-    arr = temp.split("-");
-    arr1 = parseFloat(arr[0]);
-    arr2 = parseFloat(arr[1]);
-    return (output = operate("-", arr1, arr2));
-  } else if (temp.includes("*")) {
-    arr = temp.split("*");
-    arr1 = parseFloat(arr[0]);
-    arr2 = parseFloat(arr[1]);
-    return (output = operate("*", arr1, arr2));
-  } else if (temp.includes("/")) {
-    arr = temp.split("/");
-    arr1 = parseFloat(arr[0]);
-    arr2 = parseFloat(arr[1]);
-    return (output = operate("/", arr1, arr2));
-  }
-}
+//     return (output = operate("+", arr1, arr2));
+//   } else if (temp.includes("-")) {
+//     arr = temp.split("-");
+//     arr1 = parseFloat(arr[0]);
+//     arr2 = parseFloat(arr[1]);
+//     return (output = operate("-", arr1, arr2));
+//   } else if (temp.includes("*")) {
+//     arr = temp.split("*");
+//     arr1 = parseFloat(arr[0]);
+//     arr2 = parseFloat(arr[1]);
+//     return (output = operate("*", arr1, arr2));
+//   } else if (temp.includes("/")) {
+//     arr = temp.split("/");
+//     arr1 = parseFloat(arr[0]);
+//     arr2 = parseFloat(arr[1]);
+//     return (output = operate("/", arr1, arr2));
+//   }
+// }
 
-function equalResult() {
-  if (oldCalculation.innerText == "") {
-    oldCalculation.innerText += currentCalculation.innerText;
-  } else if (oldCalculation.innerText != "") {
-    oldCalculation.innerText += ` |  ${currentCalculation.innerText}`;
-  }
-  currentCalculation.innerText = handler();
-}
+// function equalResult() {
+//   if (oldCalculation.innerText == "") {
+//     oldCalculation.innerText += currentCalculation.innerText;
+//   } else if (oldCalculation.innerText != "") {
+//     oldCalculation.innerText += ` |  ${currentCalculation.innerText}`;
+//   }
+//   currentCalculation.innerText = handler();
+// }
+
+// btnNumber.forEach(function (x) {
+//   const number = document.getElementById(x);
+//   number.addEventListener("click", () => {
+//     currentCalculation.innerText += number.innerText;
+//   });
+// });
+
+// btnOperator.forEach((x) => {
+//   const operator = document.getElementById(x);
+//   operator.addEventListener("click", () => {
+//     if (
+//       currentCalculation.innerText.includes("+") ||
+//       currentCalculation.innerText.includes("-") ||
+//       currentCalculation.innerText.includes("*") ||
+//       currentCalculation.innerText.includes("/")
+//     ) {
+//       equalResult();
+//     }
+//     currentCalculation.innerText += operator.innerText;
+//   });
+// });
+
+// btnEqual.addEventListener("click", () => {
+//   equalResult();
+// });
+
+// btnDot.addEventListener("click", () => {
+//   currentCalculation.innerText += ".";
+// });
+
+// btnAC.addEventListener("click", () => {
+//   currentCalculation.innerText = "";
+//   oldCalculation.innerText = "";
+// });
 
 btnNumber.forEach(function (x) {
   const number = document.getElementById(x);
   number.addEventListener("click", () => {
     currentCalculation.innerText += number.innerText;
+    // if (
+    //   valueHold.hasOwnProperty("operatorType") === false &&
+    //   valueHold.numberOne != ""
+    // ) {
+    //   valueHold.numberTwo += number.innerText;
+    // } else {
+    if (valueHold.hasOwnProperty("operatorType") === false) {
+      valueHold.numberOne += number.innerText;
+    } else if (valueHold.hasOwnProperty("operatorType") === true) {
+      valueHold.numberTwo += number.innerText;
+      // }
+    }
   });
 });
 
 btnOperator.forEach((x) => {
   const operator = document.getElementById(x);
   operator.addEventListener("click", () => {
-    if (
-      currentCalculation.innerText.includes("+") ||
-      currentCalculation.innerText.includes("-") ||
-      currentCalculation.innerText.includes("*") ||
-      currentCalculation.innerText.includes("/")
-    ) {
-      equalResult();
+    if (valueHold.hasOwnProperty("operatorType") === false) {
+      currentCalculation.innerText += operator.innerText;
+      valueHold.operatorType = operator.innerText;
+    } else {
+      equalize();
+      updateDisplay();
     }
-    currentCalculation.innerText += operator.innerText;
   });
 });
 
-btnEqual.addEventListener("click", () => {
-  equalResult();
-});
-
-btnDot.addEventListener("click", () => {
-  currentCalculation.innerText += ".";
-});
-
-btnAC.addEventListener("click", () => {
+function updateDisplay() {
+  oldCalculation.innerText = currentCalculation.innerText;
   currentCalculation.innerText = "";
-  oldCalculation.innerText = "";
+  currentCalculation.innerText += valueHold.result;
+  valueHold.numberOne = valueHold.result;
+  valueHold.numberOne = "";
+  valueHold.numberTwo = "";
+  valueHold.result = "";
+}
+
+function equalize() {
+  if (valueHold.hasOwnProperty("operatorType") === true) {
+    if (valueHold.operatorType.includes("+")) {
+      valueHold.result = operate(
+        "+",
+        parseFloat(valueHold.numberOne),
+        parseFloat(valueHold.numberTwo)
+      );
+      console.log(valueHold.result);
+    } else if (valueHold.operatorType.includes("-")) {
+      valueHold.result = operate(
+        "-",
+        parseFloat(valueHold.numberOne),
+        parseFloat(valueHold.numberTwo)
+      );
+      console.log(valueHold.result);
+    } else if (valueHold.operatorType.includes("*")) {
+      valueHold.result = operate(
+        "*",
+        parseFloat(valueHold.numberOne),
+        parseFloat(valueHold.numberTwo)
+      );
+      console.log(valueHold.result);
+    } else if (valueHold.operatorType.includes("/")) {
+      valueHold.result = operate(
+        "/",
+        parseFloat(valueHold.numberOne),
+        parseFloat(valueHold.numberTwo)
+      );
+      console.log(valueHold.result);
+    }
+  }
+}
+btnEqual.addEventListener("click", () => {
+  equalize();
+  updateDisplay();
 });
